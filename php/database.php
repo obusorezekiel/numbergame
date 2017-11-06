8626703490875aebd733d779ea00a075045a4e89
obusorezekiel <obusorezekiel@gmail.com>
2017-11-06 11:56:39 +0100

    Edited

--- php/database.php
+++ php/database.php
@@ -4,10 +4,5 @@ $user = "root";
 $pass = "umesi";
 $dbase = "numbergame";
 
-//$host = "webapp-mysqldbserver-786180ec-3837.mysql.database.azure.com";
-//$user = "mysqldbuser@webapp-mysqldbserver-786180ec-3837";
-//$pass = "Cloud123";
-//$dbase = "numbergame";
-
 $cxn = mysqli_connect($host,$user,$pass,$dbase) or die(mysqli_error($cxn));
 ?>
